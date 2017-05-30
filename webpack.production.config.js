14456ecd2634f4ee58d938f4b73bf2cd602e248d
Kirill Volkovich <volkovich@scand.com>
2017-05-30 20:33:21 +0300

    Update to webpack 2

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,15 +1,10 @@
 'use strict';
 const path = require('path');
 const webpack = require('webpack');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+let BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 let config = require('./webpack.development.config.js');
 config.entry = path.resolve(path.join(__dirname, 'src', 'client', 'index.js'));
-config.output = {
-  path: path.join(__dirname, "lib"),
-  libraryTarget: 'umd'
-};
-delete config.watch;
 delete config.devtool;
 delete config.output.publicPath;
 config.plugins = config.plugins.concat([
@@ -18,10 +13,8 @@ config.plugins = config.plugins.concat([
       'NODE_ENV': '"production"'
     }
   }),
-  new webpack.optimize.DedupePlugin(),
-  new webpack.optimize.OccurenceOrderPlugin(),
   new webpack.optimize.UglifyJsPlugin({
-    compressor: {
+    compress: {
       warnings: false,
       screw_ie8: true
     },
@@ -29,11 +22,9 @@ config.plugins = config.plugins.concat([
   }),
   new BundleAnalyzerPlugin({
     analyzerMode: 'server',
+    analyzerHost: '127.0.0.1',
     analyzerPort: 8888,
-    reportFilename: 'report.html',
-    generateStatsFile: false,
-    statsFilename: 'stats.json',
-    statsOptions: null
+    openAnalyzer: true
   })
 ]);
 
