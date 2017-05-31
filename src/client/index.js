80cf5952c73f66c6703d5845553687e7f8f623b4
Kirill Volkovich <volkovich@scand.com>
2017-05-31 11:06:29 +0300

    Add module bundle creation

--- src/client/index.js
+++ src/client/index.js
@@ -2,5 +2,6 @@
 
 module.exports = {
   SVG: require('./components/SVG').default,
-  SVGIcon: require('./components/SVGIcon').default
+  SVGIcon: require('./components/SVGIcon').default,
+  SVGIconsPreviewer: require('./components/SVGIconsPreviewer').default
 };
