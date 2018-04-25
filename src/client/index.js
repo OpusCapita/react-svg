c8822d004d733377c92dca8d4fbba5fef6d65a83
Dmitry Shienok <dshienok@scand.com>
2018-04-25 13:11:31 +0300

    Created new release branch. [ci skip]

--- src/client/index.js
+++
@@ -1,7 +0,0 @@
-// Here should be CommonJS component exports.
-
-module.exports = {
-  SVG: require('./components/SVG').default,
-  SVGIcon: require('./components/SVGIcon').default,
-  SVGIconsPreviewer: require('./components/SVGIconsPreviewer').default
-};
